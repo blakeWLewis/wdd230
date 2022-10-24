40394c8be1b0b750f00c6aa0b44613e0a1eda37a
blakeWLewis <lew21010@byui.edu>
2022-10-24 08:02:56 -0600

    Adding discovery page

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -1,3 +1,4 @@
+
 document.getElementById('todays-date').innerHTML = new Date().toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"}) ;
 
 const date = new Date(document.lastModified);
@@ -8,3 +9,13 @@ if (date.getDay() === 1 || date.getDay() === 2) {
 } else {
     document.querySelector('.banner').style.display = "none";
 }
+
+let temp = 40.0;
+let windSpeed = 6.0;
+let windChill = calculateWindChill(temp, windSpeed);
+
+document.getElementById('wind-speed-value').innerHTML = windSpeed
+
+document.getElementById('wind-chill-value').innerHTML = windChill
+
+document.getElementById('temprature').innerHTML = temp
